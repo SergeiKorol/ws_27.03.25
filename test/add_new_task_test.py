aff29616e9d931816767b5e4e4cfebdf8457e681
AnastasiaMadieva <anastasiamadieva7@gmail.com>
2025-03-27 22:47:12 +0500

    12312

--- test/add_new_task_test.py
+++ test/add_new_task_test.py
@@ -5,7 +5,7 @@ def test_add_new():
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     id = response.json()["id"]
     
-    assert response.status_code == 201
+    assert response.status_code == 200
 
     body = {"completed":True}
     response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
