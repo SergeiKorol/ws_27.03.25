7314dc357aacbe2892c0667c5c1f7e5e54aaef21
AnastasiaMadieva <anastasiamadieva7@gmail.com>
2025-03-27 22:11:54 +0500

    Anastasia

---
+++ test/add_new_task_test.py
@@ -0,0 +1,16 @@
+import requests
+
+def test_add_new():
+    body = {"title":"generated","completed":False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    id = response.json()["id"]
+    
+    assert response.status_code == 201
+
+    body = {"completed":True}
+    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
+    assert response.status_code == 200
+    
+    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+    assert response.status_code == 200
+    assert response.json()['completed']==True
